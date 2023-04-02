5415d256b9f3631a082af63ce1b29ec9c7a559db
soknamu <sjw04093@naver.com>
2023-04-02 21:53:33 +0900

    study

--- keras/keras31~40/keras40_LSTM1.py
+++ keras/keras31~40/keras40_LSTM1.py
@@ -8,7 +8,7 @@ datasets = np.array([1,2,3,4,5,6,7,8,9,10])
 # y = ?
 
 x = np.array([[1,2,3,], [2,3,4], [3,4,5], [4,5,6], [5,6,7], [6,7,8], [7,8,9]])
-
+#타임스탭 3임. 3개씩 쪼갰으니.
 y = np.array([4,5,6,7,8,9,10])
 
 print(x.shape, y.shape)     # (7, 3) (7,)
@@ -18,17 +18,21 @@ x = x.reshape(7, 3, 1)
 
 # 2. 모델
 model = Sequential()
-model.add(LSTM(32, input_shape=(3, 1)))
+model.add(LSTM(32, input_shape=(3, 1))) # 
 model.add(Dense(16, activation='relu'))
 model.add(Dense(1))
 
+model.summary()
+
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=1000)
+model.fit(x, y, epochs=10)
 
 # 4. 평가, 예측
 loss = model.evaluate(x, y)
-x_predict = np.array([8,9,10]).reshape(1, 3, 1)     # [[[8], [9], [10]]]
+x_predict = np.array([8,9,10]).reshape(1, 3, 1)     
+# 요약하면, x_predict 배열은 LSTM 모델에서 예상하는 입력 모양과 
+#일치시키기 위해 (1, 3)에서 (1, 3, 1)로 재구성됩니다.
 print(x_predict.shape)
 
 result = model.predict(x_predict)
