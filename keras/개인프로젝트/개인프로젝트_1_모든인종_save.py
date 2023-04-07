81493790a41521602133dc088433517f71607973
soknamu <sjw04093@naver.com>
2023-04-07 17:01:01 +0900

    ok

--- keras/개인프로젝트/개인프로젝트_1_모든인종_save.py
+++ keras/개인프로젝트/개인프로젝트_1_모든인종_save.py
@@ -2,33 +2,32 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 import time
-path = 'd:/study_data/_data/old_young/'
-save_path = 'd:/study_data/_save/old_young/'
+path = 'd:/study_data/_data/asian_data/'
+save_path = 'd:/study_data/_save/asian_data/'
 datagen = ImageDataGenerator(rescale= 1./255)
 
 start = time.time()
-old_young = datagen.flow_from_directory(path,
+asian_data = datagen.flow_from_directory(path,
             target_size=(150,150),
-            batch_size=900,
+            batch_size=12200,
             class_mode='categorical',
             color_mode= 'rgb',
             shuffle= True)
 
-old_young_x = old_young[0][0]
-old_young_y = old_young[0][1]
+asian_data_x = asian_data[0][0]
+asian_data_y = asian_data[0][1]
 
 end = time.time()
-print(end - start, 2)
-old_young_x_train, old_young_x_test, old_young_y_train, old_young_y_test = train_test_split(
-old_young_x, old_young_y, train_size= 0.7, shuffle= True, random_state=1557,stratify= old_young_y
-)
+print(round(end - start, 2))
+asian_data_x_train, asian_data_x_test, asian_data_y_train, asian_data_y_test = train_test_split(
+asian_data_x, asian_data_y, train_size= 0.7, shuffle= True, random_state=1557)
 
-print(old_young_x_train.shape) #(598, 150, 150, 3)
-print(old_young_x_test.shape)  #(257, 150, 150, 3)
-print(old_young_y_train.shape) #(598, 6)
-print(old_young_y_test.shape)  #(257, 6)
+print(asian_data_x_train.shape) #(8815, 200, 200, 3)
+print(asian_data_x_test.shape)  #(3778, 200, 200, 3)
+print(asian_data_y_train.shape) #(8815, 79)
+print(asian_data_y_test.shape)  #(3778, 79)
 
-np.save(save_path + 'old_young_x_train.npy', arr = old_young_x_train)
-np.save(save_path + 'old_young_x_test.npy', arr = old_young_x_test)
-np.save(save_path + 'old_young_y_train.npy', arr = old_young_y_train)
-np.save(save_path + 'old_young_y_test.npy', arr = old_young_y_test)
+np.save(save_path + 'asian_data_x_train.npy', arr = asian_data_x_train)
+np.save(save_path + 'asian_data_x_test.npy', arr = asian_data_x_test)
+np.save(save_path + 'asian_data_y_train.npy', arr = asian_data_y_train)
+np.save(save_path + 'asian_data_y_test.npy', arr = asian_data_y_test)
--- keras/개인프로젝트/개인프로젝트_2_모든인종_load.py
+++ keras/개인프로젝트/개인프로젝트_2_모든인종_load.py
@@ -4,19 +4,18 @@ from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D,BatchNo
 import time
 from tensorflow.keras.callbacks import EarlyStopping
 #1. 데이터
-save_path = 'd:/study_data/_save/old_young/'
+save_path = 'd:/study_data/_save/asian_data/'
 st =time.time()
 
-old_young_x_train = np.load(save_path + 'old_young_x_train.npy')
-old_young_x_test = np.load(save_path + 'old_young_x_test.npy')
-old_young_y_train = np.load(save_path + 'old_young_y_train.npy')
-old_young_y_test = np.load(save_path + 'old_young_y_test.npy')
+asian_data_x_train = np.load(save_path + 'asian_data_x_train.npy')
+asian_data_x_test = np.load(save_path + 'asian_data_x_test.npy')
+asian_data_y_train = np.load(save_path + 'asian_data_y_train.npy')
+asian_data_y_test = np.load(save_path + 'asian_data_y_test.npy')
 
 #2.모델구성
 model = Sequential()
-model.add(Conv2D(128, (3,3), input_shape=(150,150,3), activation= 'relu'))
+model.add(Conv2D(128, (3,3), input_shape=(200,200,3), activation= 'relu'))
 model.add(MaxPooling2D())
-model.add(BatchNormalization())
 model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
 model.add(MaxPooling2D())
 model.add(Conv2D(64, (3,3), padding='same', activation= 'relu'))
@@ -24,16 +23,16 @@ model.add(Flatten())
 model.add(Dense(32,activation='relu'))
 model.add(Dense(16,activation='relu'))
 model.add(Dense(8,activation='relu'))
-model.add(Dense(6,activation='softmax'))
+model.add(Dense(79,activation='softmax'))
 
 #3.컴파일, 훈련
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics= ['acc'])
 es = EarlyStopping(monitor='val_acc', mode='max', verbose=1, restore_best_weights=True, patience=500)
-hist = model.fit(old_young_x_train, old_young_y_train, epochs = 5000, validation_data=(old_young_x_test, old_young_y_test),
+hist = model.fit(asian_data_x_train, asian_data_y_train, epochs = 50, validation_data=(asian_data_x_test, asian_data_y_test),
                  callbacks = [es])
 
 ed = time.time()
-print(ed - st ,2)
+print(np.round(ed - st ,2))
 
 loss = hist.history['loss']
 val_loss = hist.history['val_loss']
@@ -41,11 +40,11 @@ acc = hist.history['acc']
 val_acc = hist.history['val_acc']
 
 #4.평가, 예측
-loss = model.evaluate(old_young_x_test, old_young_y_test)
+loss = model.evaluate(asian_data_x_test, asian_data_y_test)
 print('loss : ', loss)
 
-y_pred = np.argmax(model.predict(old_young_x_test),axis = 1)
-y_test = np.argmax(old_young_y_test, axis=1)
+y_pred = np.argmax(model.predict(asian_data_x_test),axis = 1)
+y_test = np.argmax(asian_data_y_test, axis=1)
 from sklearn.metrics import accuracy_score
 
 acc = accuracy_score(y_test, y_pred)
