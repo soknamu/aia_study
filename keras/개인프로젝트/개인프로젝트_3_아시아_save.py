2b06a5c908a1960ea1d5c0fd439f231686903811
soknamu <sjw04093@naver.com>
2023-04-11 00:54:24 +0900

    ggg

--- keras/개인프로젝트/개인프로젝트_3_아시아_save.py
+++ keras/개인프로젝트/개인프로젝트_3_아시아_save.py
@@ -2,14 +2,14 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 import time
-path = 'd:/study_data/_data/young_old/'
-save_path = 'd:/study_data/_save/young_old/'
+path = 'd:/study_data/_data/asian_data/'
+save_path = 'd:/study_data/_save/asian_data/'
 datagen = ImageDataGenerator(rescale= 1./255)
 
 start = time.time()
 young_old = datagen.flow_from_directory(path,
             target_size=(150,150),
-            batch_size=900,
+            batch_size=10070,
             class_mode='categorical',
             color_mode= 'rgb',
             shuffle= True)
@@ -23,10 +23,10 @@ young_old_x_train, young_old_x_test, young_old_y_train, young_old_y_test = train
 young_old_x, young_old_y, train_size= 0.7, shuffle= True, random_state=1557
 )
 
-print(young_old_x_train.shape) #(598, 150, 150, 3)
-print(young_old_x_test.shape)  #(257, 150, 150, 3)
-print(young_old_y_train.shape) #(598, 6)
-print(young_old_y_test.shape)  #(257, 6)
+print(young_old_x_train.shape) #(7049, 150, 150, 3)
+print(young_old_x_test.shape)  #(3021, 150, 150, 3)
+print(young_old_y_train.shape) #(7049, 79)
+print(young_old_y_test.shape)  #(3021, 79)
 
 np.save(save_path + 'young_old_x_train.npy', arr = young_old_x_train)
 np.save(save_path + 'young_old_x_test.npy', arr = young_old_x_test)
--- keras/개인프로젝트/폴더전체이미지삭제.py
+++ keras/개인프로젝트/폴더전체이미지삭제.py
@@ -2,36 +2,41 @@ import os
 import numpy as np
 from PIL import Image
 from rembg import remove
+import time
 
-input_dir = "./asian_data/4/"
-output_dir = "d:study_data/_data/asian_data/4/"
+for i in range(53,81):
+    input_dir = f"d:study_data/asian_data/{i}/"
+    output_dir = f"d:study_data/_data/asian_data/{i}/"
+    print(f'{i}번 폴더 시작')
+    start_time=time.time()
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-# Create the output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    # Loop over all the files in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Load the image
+            img_path = os.path.join(input_dir, filename)
+            img = Image.open(img_path)
 
-# Loop over all the files in the input directory
-for filename in os.listdir(input_dir):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        # Load the image
-        img_path = os.path.join(input_dir, filename)
-        img = Image.open(img_path)
+            # Remove the background
+            out = remove(img)
 
-        # Remove the background
-        out = remove(img)
+            # Convert the output image to RGBA mode
+            out = out.convert('RGBA')
 
-        # Convert the output image to RGBA mode
-        out = out.convert('RGBA')
+            # Create a NumPy array from the image
+            arr = np.array(out)
 
-        # Create a NumPy array from the image
-        arr = np.array(out)
+            # Set the alpha channel to 0 for pixels below a threshold value (in this case, 100)
+            arr[(arr[:,:,0] < 1) & (arr[:,:,1] < 1) & (arr[:,:,2] < 1)] = [0, 0, 0, 0]
 
-        # Set the alpha channel to 0 for pixels below a threshold value (in this case, 100)
-        arr[(arr[:,:,0] < 1) & (arr[:,:,1] < 1) & (arr[:,:,2] < 1)] = [0, 0, 0, 0]
+            # Create a new Image object from the modified array
+            out = Image.fromarray(arr)
 
-        # Create a new Image object from the modified array
-        out = Image.fromarray(arr)
-
-        # Save the image with a transparent background
-        output_path = os.path.join(output_dir, filename.split(".")[0] + "_transparent.png")
-        out.save(output_path)
+            # Save the image with a transparent background
+            output_path = os.path.join(output_dir, filename.split(".")[0] + "_transparent.png")
+            out.save(output_path)
+    print(f'{i}번 폴더 끝')
+    print(f'runtime : {time.time()-start_time}')
