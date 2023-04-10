2dbe365b1064d5635a1a98bbd7ce4811ecd31af5
soknamu <126450951+soknamu@users.noreply.github.com>
2023-04-10 17:51:35 +0900

    Add files via upload

--- keras/개인프로젝트/폴더전체이미지삭제.py
+++ keras/개인프로젝트/폴더전체이미지삭제.py
@@ -3,35 +3,37 @@ import numpy as np
 from PIL import Image
 from rembg import remove
 
-input_dir = "./asian_data/4/"
-output_dir = "d:study_data/_data/asian_data/4/"
 
-# Create the output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+for i in range(2,81):
+    input_dir = f"d:study_data/asian_data/{i}/"
+    output_dir = f"d:study_data/_data/asian_data/{i}/"
 
-# Loop over all the files in the input directory
-for filename in os.listdir(input_dir):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        # Load the image
-        img_path = os.path.join(input_dir, filename)
-        img = Image.open(img_path)
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-        # Remove the background
-        out = remove(img)
+    # Loop over all the files in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Load the image
+            img_path = os.path.join(input_dir, filename)
+            img = Image.open(img_path)
 
-        # Convert the output image to RGBA mode
-        out = out.convert('RGBA')
+            # Remove the background
+            out = remove(img)
 
-        # Create a NumPy array from the image
-        arr = np.array(out)
+            # Convert the output image to RGBA mode
+            out = out.convert('RGBA')
 
-        # Set the alpha channel to 0 for pixels below a threshold value (in this case, 100)
-        arr[(arr[:,:,0] < 1) & (arr[:,:,1] < 1) & (arr[:,:,2] < 1)] = [0, 0, 0, 0]
+            # Create a NumPy array from the image
+            arr = np.array(out)
 
-        # Create a new Image object from the modified array
-        out = Image.fromarray(arr)
+            # Set the alpha channel to 0 for pixels below a threshold value (in this case, 100)
+            arr[(arr[:,:,0] < 1) & (arr[:,:,1] < 1) & (arr[:,:,2] < 1)] = [0, 0, 0, 0]
 
-        # Save the image with a transparent background
-        output_path = os.path.join(output_dir, filename.split(".")[0] + "_transparent.png")
-        out.save(output_path)
+            # Create a new Image object from the modified array
+            out = Image.fromarray(arr)
+
+            # Save the image with a transparent background
+            output_path = os.path.join(output_dir, filename.split(".")[0] + "_transparent.png")
+            out.save(output_path)
