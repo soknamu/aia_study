5b489f22e59aca7b15b6c01ef69b8c3b45971de1
soknamu <sjw04093@naver.com>
2023-04-20 19:22:51 +0900

    ff

--- python/dasfasfas.py
+++ python/dasfasfas.py
@@ -8,6 +8,7 @@ from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import f1_score
 from sklearn.neighbors import LocalOutlierFactor
+
 # Load data
 path = './_data/air/'
 save_path = './_save/air/'
@@ -36,25 +37,22 @@ X_train_norm = scaler.fit_transform(X_train)
 X_val_norm = scaler.transform(X_val)
 test_data_norm = scaler.transform(test_data[features])
 
-# lof = LocalOutlierFactor(n_neighbors=20, contamination=0.45999,
-#                          leaf_size=97,
-#                          algorithm='auto',
-#                          metric='chebyshev',
-#                          metric_params= None,
-#                          novelty= False,
-#                          p=300)
+# Perform local outlier factor (LOF) to identify anomalies in train data
+lof = LocalOutlierFactor(n_neighbors=20, contamination=0.45999,
+                         leaf_size=97,
+                         algorithm='auto',
+                         metric='chebyshev',
+                         metric_params=None,
+                         novelty=False,
+                         p=300)
+y_pred_train = lof.fit_predict(X_train_norm)
 
-# y_pred_train_turnd = lof.fit_predict(X_val)
-
-# #이상치탐지
-# test_data_lof = scaler.transform(test_data[features])
-# y_pred_test_lof = lof.fit_predict(test_data_lof)
-# lof_predictions = [1 if x == -1 else 0 for x in y_pred_test_lof]
+# Remove train anomalies
+X_train_norm = X_train_norm[y_pred_train == 1]
 
 # Define model architecture
 model = Sequential()
 model.add(Dense(128, activation='relu', input_dim=X_train_norm.shape[1]))
-model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(X_train_norm.shape[1], activation='linear'))
@@ -63,7 +61,7 @@ model.add(Dense(X_train_norm.shape[1], activation='linear'))
 model.compile(loss='mse', optimizer='adam')
 
 # Define early stopping callback
-early_stop = EarlyStopping(monitor='val_loss', patience=505)
+early_stop = EarlyStopping(monitor='val_loss', patience=500)
 
 # Train model
 history = model.fit(X_train_norm, X_train_norm, epochs=50000, batch_size=50, validation_data=(X_val_norm, X_val_norm), callbacks=[early_stop])
@@ -71,18 +69,15 @@ history = model.fit(X_train_norm, X_train_norm, epochs=50000, batch_size=50, val
 # Predict anomalies on test data
 test_preds = model.predict(test_data_norm)
 errors = np.mean(np.power(test_data_norm - test_preds, 2), axis=1)
-y_pred = np.where(errors >= np.percentile(errors, 98), 1, 0)
+
+# Perform local outlier factor (LOF) to identify anomalies in test data
+y_pred_test_lof = lof.fit_predict(test_data_norm)
+lof_predictions = [1 if x == -1 else 0 for x in y_pred_test_lof]
+
+# Label anomalies as 1 and non-anomalies as 0
+y_pred = np.where((errors >= np.percentile(errors, 88)) | (lof_predictions == 1), 1, 0)
 
 # Save submission file
 submission['label'] = pd.DataFrame({'Prediction': y_pred})
 date = datetime.datetime.now().strftime("%m%d_%H%M%S")
 submission.to_csv(save_path + 'air_' + date + '.csv', index=False)
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-print(test_data.corr())
-plt.figure(figsize= (10,8))
-sns.set(font_scale=1.2)
-sns.heatmap(train_data.corr(), square=True, annot = True, cbar=True)
-plt.show()
