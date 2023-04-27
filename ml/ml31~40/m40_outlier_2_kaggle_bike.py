24f54841fa22e52991b3f95fb1ba35ddccbce6c0
soknamu <sjw04093@naver.com>
2023-04-27 09:27:04 +0900

    kk

---
+++ ml/ml31~40/m40_outlier_2_kaggle_bike.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.covariance import EllipticEnvelope
+import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import LabelEncoder
+
+#1. 데이터
+path = './_data/kaggle_bike/'
+path_save = './_save/kaggle_bike/'
+train_csv = pd.read_csv(path + 'train.csv', index_col=0)
+test_csv = pd.read_csv(path + 'test.csv', index_col=0)
+
+x = train_csv.drop(['count','casual','registered'], axis =1)
+
+y = train_csv['count']
+
+
+def outliers(data_out):
+    quartile_1, q2, quartile_3 = np.percentile(data_out, [25, 50, 75], axis=0)
+    print('1사분위 : ', quartile_1) 
+    print('q2 : ', q2) 
+    print('3사분위 : ', quartile_3) 
+    iqr = quartile_3 - quartile_1 
+    print('iqr : ', iqr)
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5) 
+    return np.where((data_out>upper_bound) | (data_out<lower_bound))
+outliers_loc = outliers(x)
+print('이상치의 위치 : ', list((outliers_loc)))
+x[outliers_loc] = 99999999999
+
+#결측치 제거
+imputer = IterativeImputer(estimator=XGBRegressor())
+x = imputer.fit_transform(x)
+
+xgb = XGBRegressor()
+xgb.fit(x, y)
+results = xgb.score(x,y)
+y_submit = xgb.predict(test_csv)
+print(results)
+
+submission = pd.read_csv(path + 'submission.csv', index_col=0)
+submission['count'] = y_submit
+submission.to_csv(path_save + 'subway.csv')
