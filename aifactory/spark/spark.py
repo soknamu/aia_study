a561cf28a8c47024d7dcf9a8707d0b38d7c2d230
soknamu <sjw04093@naver.com>
2023-04-27 17:59:23 +0900

    ff

--- aifactory/spark/spark.py
+++ aifactory/spark/spark.py
@@ -3,9 +3,10 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
-path_train = './_data/spark/TRAIN/'
+path = './_data/spark/TRAIN/'
+path_sub = './_save/spark/'
+path_save = './_save/spark/'
 
-path = './_save/spark/'
-
-train_csv = pd.read_csv(path_train + 'train_all.csv', index_col=0)
-test_csv = pd.read_csv(path + 'train.csv', index_col=0)
+train_csv = pd.read_csv(path + 'train.csv', index_col=0)
+test_csv = pd.read_csv(path_save + 'test_input.csv', index_col=0)
+submission = pd.read_csv(path_sub+'answer_sample.csv')
