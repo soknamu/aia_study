6e338370a79206b93a202b00dad04faf9f137b73
soknamu <sjw04093@naver.com>
2023-04-29 12:14:08 +0900

    대회

--- dacon/airplane/fafa copy.py
+++ dacon/airplane/fafa copy.py
@@ -6,55 +6,72 @@ from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKF
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, make_scorer
 from xgboost import XGBClassifier
 import time
+
 # Load data
 train = pd.read_csv('c:/study/_data/dacon_airplane/train.csv')
 test = pd.read_csv('c:/study/_data/dacon_airplane/test.csv')
 sample_submission = pd.read_csv('c:/study/_data/dacon_airplane/sample_submission.csv', index_col=0)
 
+# Define the function to replace outliers with NaN
+def replace_outliers_with_nan(data, column, threshold):
+    # Calculate z-scores for the column
+    z_scores = np.abs((data[column] - data[column].mean()) / data[column].std())
+
+    # Replace values greater than the threshold with NaN
+    data[column][z_scores > threshold] = np.nan
+
+    return data
+
+# Replace outliers in the specified column with NaN values
+train = replace_outliers_with_nan(train, ['Estimated_Departure_Time',
+                                          'Estimated_Arrival_Time',
+                                          'Origin_State',
+                                          'Destination_State',
+                                          'Airline',
+                                          'Carrier_Code(IATA)',
+                                          'Carrier_ID(DOT)'], 3)
 
 
-
-print(train)
 # Replace variables with missing values except for the label (Delay) with the most frequent values of the training data
 NaN = ['Origin_State', 'Destination_State', 'Airline', 'Estimated_Departure_Time', 'Estimated_Arrival_Time', 'Carrier_Code(IATA)', 'Carrier_ID(DOT)']
+qual_col = ['Origin_Airport', 'Origin_State', 'Destination_Airport', 'Destination_State', 'Airline', 'Carrier_Code(IATA)', 'Tail_Number']
 
+# Concatenate the training and test sets
+concatenated = pd.concat([train.drop('Delay', axis=1), test])
+
+# Fit the label encoder on the concatenated set
+for i in qual_col:
+    le = LabelEncoder()
+    le = le.fit(concatenated[i])
+    train[i] = le.transform(train[i])
+    test[i] = le.transform(test[i])
+
+# Replace missing values with the mean of each column
 for col in NaN:
-    mode = train[col].mode()[0]
-    train[col] = train[col].fillna(mode)
+    concatenated[col] = pd.to_numeric(concatenated[col], errors='coerce')
+    mean = concatenated[col].mean() # calculate mean of the column
+    train[col] = train[col].fillna(mean) # fill missing values with mean
+    test[col] = test[col].fillna(mean)
 
-    if col in test.columns:
-        test[col] = test[col].fillna(mode)
 print('Done.')
 
 # Quantify qualitative variables
-qual_col = ['Origin_Airport', 'Origin_State', 'Destination_Airport', 'Destination_State', 'Airline', 'Carrier_Code(IATA)', 'Tail_Number']
-
-for i in qual_col:
-    le = LabelEncoder()
-    le = le.fit(train[i])
-    train[i] = le.transform(train[i])
-
-    for label in np.unique(test[i]):
-        if label not in le.classes_:
-            le.classes_ = np.append(le.classes_, label)
-    test[i] = le.transform(test[i])
-print('Done.')
 
 # Remove unlabeled data
-train = train.dropna()
+#train = train.dropna()
 
-column4 = {}
-for i, column in enumerate(sample_submission.columns):
-    column4[column] = i
+# column4 = {}
+# for i, column in enumerate(sample_submission.columns):
+#     column4[column] = i
 
-def to_number(x, dic):
-    return dic[x]
+# def to_number(x, dic):
+#     return dic[x]
 
-train.loc[:, 'Delay_num'] = train['Delay'].apply(lambda x: to_number(x, column4))
-print('Done.')
+# train.loc[:, 'Delay_num'] = train['Delay'].apply(lambda x: to_number(x, column4))
+# print('Done.')
 
-train_x = train.drop(columns=['ID', 'Delay', 'Delay_num'])
-train_y = train['Delay_num']
+train_x = train.drop(columns=['ID', 'Delay'])
+train_y = train['Delay']
 test_x = test.drop(columns=['ID'])
 
 # Split the training dataset into a training set and a validation set
@@ -68,7 +85,6 @@ test_x = scaler.transform(test_x)
 
 # Cross-validation with StratifiedKFold
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
 # Model and hyperparameter tuning using GridSearchCV
 model = XGBClassifier(random_state=42)
 
@@ -81,7 +97,7 @@ param_grid = {
 grid = GridSearchCV(model,
                     param_grid,
                     cv=cv,
-                    scoring='accuracy',
+                    scoring='accuarcy',
                     n_jobs=-1,
                     verbose=1)
 
